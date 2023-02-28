f18075cda446550151c71b47ec2d803a4039a496
[Andrew Skvarcius] <andrewskvarcius@gmail.com>
2023-02-27 20:55:56 -0500

    moved to root

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
-//   PART ONE
-//#1 Dom is ready (party time)
-$(document).ready(console.log('Let’s get ready to party with jQuery!'));
+const $gifParty = $("#gif-party");
+const $searchInput = $("#search");
 
-//#2 all images of an article tag the class of image-ceter
-$('article img').addClass('image-cnter'); 
 
-//#3 remove last p in article
-let $articleItem = $('article p');
-$articleItem.eq(5).remove(); 
-//#4 set font size to be random pixel size
-$('#title').css('font-size', Math.random() * 100)
-//#5 add list item
-$('li').append('<li> That cup has spots</li>');
+function getGif(res) {
+    let numResult = res.data.length;
+    if(numResult){
+        let randomId = Math.floor(Math.random() * numResult);
+        let $newColumn = $("<div>" , {class: "col-md-4 col-12-mb-4"});
+        let $newGif = $("<img>", {
+            src: res.data[randomId].images.original.url, 
+            class: "w-100"
+        });
+        $newColumn.append($newGif);
+        $gifParty.append($newColumn);
+    }
+    
+  }
+  
 
-//#6 Empty the aside and put a paragraph in it apologizing for the list’s existence
-$('aside').empty().append('<p> That list was awful Sorry.</p>');
 
-//#7 change the numbers in the three inputs on the bottom, the background color of the 
-//body should change to match whatever the three values in the inputs are.
-$(".form-control").on('keyup blur change', function() {
-    let red = $(".form-control").eq(0).val();
-    let blue = $(".form-control").eq(1).val();
-    let green = $(".form-control").eq(2).val();
-    $("body").css("background-color", "rgb(" + red + "," + green + ", " + blue + ")")
-});
-//#8 remove img
-$("img").on('click', function (e) {
-    $(e.target).remove();
+$("form").on("submit",  async function(e) {
+    e.preventDefault();
+ 
+    let searchTerm = $searchInput.val();
+     $searchInput.val("");
+     
+
+   const response = await axios.get("http://api.giphy.com/v1/gifs/search", {
+    params: {
+      q: searchTerm,
+      api_key: "rGMfB1a1rCIAl80Rn2pXxdTIAaMHH0ut"
+    }
   });
+  //console.log(response)
+  getGif(response.data);
+});
+
+$("#remove").on("click", function() {
+    $gifParty.empty();
+  });
+   
